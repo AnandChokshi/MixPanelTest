f4cc905d10a6f43738f98f945cb5336099eb1b81
Anand A Chokshi <a_chokshi@hotmail.com>
2015-07-10 16:57:11 -0700

    With Props and delay

--- app/src/main/java/com/inventure/test/aftest/MainActivity.java
+++ app/src/main/java/com/inventure/test/aftest/MainActivity.java
@@ -7,10 +7,14 @@ import android.telephony.TelephonyManager;
 
 import com.mixpanel.android.mpmetrics.MixpanelAPI;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends Activity {
 
     MixpanelAPI mMixpanel;
+    JSONObject props;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +22,29 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         String projectToken = "7065ba64bf3b84db62283dafe92d2b72";
-        mMixpanel = MixpanelAPI.getInstance(this, projectToken);
-        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        mMixpanel = MixpanelAPI.getInstance(getBaseContext(), projectToken);
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         String deviceId = telephonyManager.getDeviceId();
         mMixpanel.identify(deviceId);
-        mMixpanel.track("AppInstall");
-    }
 
-    @Override
-    protected void onDestroy() {
-        mMixpanel.flush();
-        super.onDestroy();
+        // With props
+        try {
+            props = new JSONObject();
+            props.put("TEST", "WITH PROPS");
+            mMixpanel.track("AppInstall", props);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        // Delay event to see if it works or not
+        android.os.Handler handler = new android.os.Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mMixpanel.track("DelayInstall", props);
+            }
+        }, 5000);
+
     }
 }
